9c7b5ed23afe965969b2544c1a44e618488e4c85
babilinx <babilinx.evx1o@simplelogin.com>
2024-12-24 14:49:20 +0100

    feat(i18n): add l10n template for branding

---
+++ patches/calamares-nixos-extensions/branding/nixos/lang/calamares-nixos_template.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>show</name>
+    <message>
+        <location filename="../show.qml" line="36"/>
+        <source>A community of players</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../show.qml" line="54"/>
+        <source>The tools you need to play are natively integrated.&lt;br/&gt;We also tested the applications needed by content creators for their work. &lt;br/&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../show.qml" line="66"/>
+        <source>Based on NixOS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../show.qml" line="84"/>
+        <source>Based on NixOS, installing or upgrading one package cannot break other packages.&lt;br/&gt;You can easily roll back to previous versions when you want.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../show.qml" line="114"/>
+        <source>GLF OS is fast. Really.&lt;br/&gt;It has been designed from the beginning for optimal out-of-the-box performance.&lt;br/&gt;It was designed from the outset to deliver optimum performance straight out of the box.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
