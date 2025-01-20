89089883fcd803adcbe0e019ad4bb4ce961ac0a3
liberodark <liberodark@gmail.com>
2025-01-20 19:51:42 +0100

    Add SCX
    I propose to use directly SCX for improve performance of gaming.

--- nix-cfg/glf/system.nix
+++ nix-cfg/glf/system.nix
@@ -8,6 +8,13 @@
     enable32Bit = true;
   };
 
+  # Enable SCX
+  services.scx = {
+    enable = true;
+    package = pkgs.scx.full;
+    scheduler = "scx_bpfland";
+  };
+
   system.autoUpgrade = { enable = true; dates = "weekly"; };
 
   nixpkgs = { config = { allowUnfree = true; }; };
