721ed4de2a5ad18f7c6a70682b613448a9bc6092
sebanc <sebanc.xda@gmail.com>
2025-03-25 07:28:46 +0100

    Add nvidia proprietary driver support and related specialisation

--- iso-cfg/configuration.nix
+++ iso-cfg/configuration.nix
@@ -4,6 +4,15 @@
 
   glf.autoUpgrade = lib.mkForce false;
   glf.nvidia_config.enable = true;
+  system.nixos.tags = lib.mkDefault [ "Alpha.v2" ];
+  specialisation = {
+    nvidia_proprietary_driver = {
+      configuration = {
+        system.nixos.tags = lib.mkForce [ "nvidia_proprietary_driver-Alpha.v2" ];
+        glf.nvidia_config.type = "proprietary";
+      };
+    };
+  };
 
   i18n.defaultLocale = "fr_FR.UTF-8";
 
