302cd73f7d598581c86c5288a83adb56ee4757ae
Cammi <118620964+camini@users.noreply.github.com>
2025-03-27 11:09:36 +0100

    Update gaming.nix

--- modules/default/gaming.nix
+++ modules/default/gaming.nix
@@ -38,20 +38,24 @@
     '';
 
     hardware.steam-hardware.enable = true;
-    programs.steam.gamescopeSession.enable = true;
-
-    programs.steam = {
-      enable = true;
-      package = pkgs.steam.override {
-        extraEnv = {
-          MANGOHUD = true;
-          OBS_VKCAPTURE = true;
-        };
-      };
-      remotePlay.openFirewall = true;
-      localNetworkGameTransfers.openFirewall = true;
-      extraCompatPackages = with pkgs; [ proton-ge-bin ];
-    };
+    programs = {
+  gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
+  steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+  };
+};
+hardware.xone.enable = true; # support for the xbox controller USB dongle
+services.getty.autologinUser = "your_user";
+environment = {
+  systemPackages = [ pkgs.mangohud ];
+  loginShellInit = ''
+    [[ "$(tty)" = "/dev/tty1" ]] && ./gs.sh
+  '';
+};
 
   };
 
