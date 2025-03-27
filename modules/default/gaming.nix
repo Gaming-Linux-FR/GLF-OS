18246adf24c9561d0d1231ec040ab215f972d629
Cammi <118620964+camini@users.noreply.github.com>
2025-03-27 11:11:38 +0100

    Update gaming.nix

--- modules/default/gaming.nix
+++ modules/default/gaming.nix
@@ -51,7 +51,6 @@
 hardware.xone.enable = true; # support for the xbox controller USB dongle
 services.getty.autologinUser = "your_user";
 environment = {
-  systemPackages = [ pkgs.mangohud ];
   loginShellInit = ''
     [[ "$(tty)" = "/dev/tty1" ]] && ./gs.sh
   '';
