be4ebd3b96ac91f65b6bd0b81e80ecee39446c73
Cammi <118620964+camini@users.noreply.github.com>
2025-03-29 14:44:13 +0100

    Update boot.nix

--- modules/default/boot.nix
+++ modules/default/boot.nix
@@ -19,8 +19,6 @@
       tmp.cleanOnBoot = true;
       #supportedFilesystems.zfs = lib.mkDefault false;
       kernelPackages = lib.mkDefault pkgs.linuxPackages_zen;
-      initrd.kernelModules =
-        if builtins.elem "kvm-amd" config.boot.kernelModules then [ "amdgpu" ] else [ ]; 
       kernelParams =
         if builtins.elem "kvm-amd" config.boot.kernelModules then [ "amd_pstate=active" ] else [ ];
       plymouth = {
