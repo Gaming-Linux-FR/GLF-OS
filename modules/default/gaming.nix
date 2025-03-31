a882d799391548e109290e3d035d61768d1a4b23
Vinceff <101025517+vinceff@users.noreply.github.com>
2025-03-31 16:36:05 +0200

    mango revisite

--- modules/default/gaming.nix
+++ modules/default/gaming.nix
@@ -25,7 +25,8 @@
 
     environment.sessionVariables = {
       STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
-      MANGOHUD_CONFIG = "control=mangohud,legacy_layout=0,horizontal,battery,time,time_format=%H\\:%M,gpu_stats,gpu_power,cpu_stats,ram,vram,fps,frametime=1,frame_timing=1,hud_no_margin,table_columns=14";
+      MANGOHUD_CONFIG="text=GLF OS,text_color=ffffff,text_size=20,gpu_temp,kernel,driver,control=mangohud,legacy_layout=0,horizontal,battery,time,time_format=%H\\:%M,gpu_stats,gpu_power,cpu_stats,ram,vram,fps,frametime=1,frame_timing=1,hud_no_margin,table_columns=14,font_size=12,position=top-right"
+
     };
 
     services.udev.extraRules = ''
