3ec475b590770911463930f7aaa1ca65e5f61414
Vinceff <101025517+vinceff@users.noreply.github.com>
2025-04-01 22:20:53 +0200

    Update flake.nix

--- iso-cfg/flake.nix
+++ iso-cfg/flake.nix
@@ -22,6 +22,12 @@
           inherit system;
           config.allowUnfree = true;
         };
+      unstablePkgs =
+        system:
+        import nixpkgs-unstable {
+          inherit system;
+          config.allowUnfree = true;
+        };
     in
     {
       nixosConfigurations."GLF-OS" = nixpkgs.lib.nixosSystem {
@@ -30,11 +36,7 @@
           ./configuration.nix
           inputs.glf.nixosModules.default
           {
-            config.nixpkgs.unstable = import nixpkgs-unstable { # Fournir nixpkgs.unstable
-              inherit system;
-              config.allowUnfree = true;
-            };
-            environment.systemPackages = with config.nixpkgs.unstable.legacyPackages.${system}; [ # Utiliser config.nixpkgs.unstable
+            environment.systemPackages = with (unstablePkgs "x86_64-linux").legacyPackages; [ # Utiliser unstablePkgs
               heroic
               lutris
             ];
