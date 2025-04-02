8805952ca2f02d818e62793ebd7885370faa87fa
Vinceff <101025517+vinceff@users.noreply.github.com>
2025-04-02 15:24:54 +0200

    Update flake.nix

--- iso-cfg/flake.nix
+++ iso-cfg/flake.nix
@@ -1,65 +1,70 @@
-# flake.nix (Celui utilisé par Calamares pour installer)
+# flake.nix (Dans iso-cfg/, corrigé pour utiliser les modules du flake racine)
 {
-  description = "GLF-OS ISO Configuration";
+  description = "GLF-OS ISO Configuration - Installer Flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    glf.url = "github:vinceff/GLF-OS-stable/glf-os-stable";
+
+    # Input pointant vers le flake racine (situé dans le dossier parent ../)
+    # 'follows' assure la cohérence de nixpkgs entre les deux flakes.
+    # Vérifiez que le chemin "path:../" est correct !
+    glfRoot = { url = "path:../"; follows = "nixpkgs"; };
   };
 
   outputs =
     {
       nixpkgs,
       nixpkgs-unstable,
-      glf, 
+      glfRoot, # Nouvel input pour accéder au flake racine
+      # self, # Ajouter si besoin
       ...
     }:
     let
       system = "x86_64-linux";
-      pkgsSettings =
-        system:
-        import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-      unstablePkgsImport = 
-        import nixpkgs-unstable {
-          inherit system;
-          config.allowUnfree = true;
-        };
+      nixpkgsConfig = { allowUnfree = true; };
 
-      # Définir la variable ici en utilisant l'argument 'glf'
-      glfDefaultModules = glf.nixosModules.default;
+      # Import stable pour la base pkgs
+      pkgs = import nixpkgs {
+        inherit system;
+        config = nixpkgsConfig;
+      };
 
-      # Définir le jeu de paquets unstable pour l'utiliser ci-dessous
-      # et potentiellement dans specialArgs
-      pkgs-unstable = unstablePkgsImport.legacyPackages.${system};
+      # Import unstable pour pkgs-unstable
+      unstablePkgsImport = import nixpkgs-unstable {
+        inherit system;
+        config = nixpkgsConfig;
+      };
+      # *** CORRECTION legacyPackages ICI ***
+      pkgs-unstable = unstablePkgsImport; # Le résultat de l'import est déjà le bon set
+
+      # *** CORRECTION Module Import ICI ***
+      # Récupère les modules depuis la sortie 'nixosModules.default' du flake racine
+      # (le flake racine doit exporter nixosModules = { default = ./modules/default; };)
+      glfDefaultModules = glfRoot.nixosModules.default;
 
     in
     {
+      # Définit la configuration NixOS que Calamares doit installer
       nixosConfigurations."GLF-OS" = nixpkgs.lib.nixosSystem {
         # Paquets de base venant de nixpkgs (stable 24.11)
-        pkgs = pkgsSettings "x86_64-linux";
+        inherit pkgs;
 
-        # Argument spécial pour rendre pkgs-unstable dispo aux modules GLF
-        # (si gaming.nix, etc., en ont besoin)
+        # Arguments spéciaux à passer aux modules (venant du flake racine)
         specialArgs = {
-           # Passer le jeu de paquets unstable
+           # Passer le jeu de paquets unstable correctement défini
            inherit pkgs-unstable;
-           
-           # inherit glf; # Si les modules GLF ont besoin d'accéder à l'input glf
+           # On pourrait aussi passer l'input racine si les modules en ont besoin
+           # inherit glfRoot;
         };
 
         modules = [
+          # Configuration locale générée par Calamares sur la cible
+          # (Ce fichier contient les choix de l'utilisateur : partitionnement, user, etc)
           ./configuration.nix
+
+          # Modules GLF venant du flake racine (qui gère pkgs-unstable via specialArgs)
           glfDefaultModules
-          # {
-          #   environment.systemPackages = with pkgs-unstable; [
-          #     heroic
-          #     lutris
-          #   ];
-          # }
         ];
       };
     };
