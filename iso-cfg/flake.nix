2c53a1cf1f2c0278258b43a70e113f1452943da9
Vinceff <101025517+vinceff@users.noreply.github.com>
2025-04-02 15:32:43 +0200

    Update flake.nix

--- iso-cfg/flake.nix
+++ iso-cfg/flake.nix
@@ -1,4 +1,4 @@
-# flake.nix (Dans iso-cfg/, corrigé pour utiliser les modules du flake racine)
+# flake.nix 
 {
   description = "GLF-OS ISO Configuration - Installer Flake";
 
@@ -6,9 +6,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    # Input pointant vers le flake racine (situé dans le dossier parent ../)
-    # 'follows' assure la cohérence de nixpkgs entre les deux flakes.
-    # Vérifiez que le chemin "path:../" est correct !
+    # 'follows' assure la cohérence de nixpkgs.
     glfRoot = { url = "path:../"; follows = "nixpkgs"; };
   };
 
@@ -16,8 +14,8 @@
     {
       nixpkgs,
       nixpkgs-unstable,
-      glfRoot, # Nouvel input pour accéder au flake racine
-      # self, # Ajouter si besoin
+      glfRoot, 
+     
       ...
     }:
     let
@@ -35,33 +33,26 @@
         inherit system;
         config = nixpkgsConfig;
       };
-      # *** CORRECTION legacyPackages ICI ***
-      pkgs-unstable = unstablePkgsImport; # Le résultat de l'import est déjà le bon set
+   
+      pkgs-unstable = unstablePkgsImport;
 
-      # *** CORRECTION Module Import ICI ***
-      # Récupère les modules depuis la sortie 'nixosModules.default' du flake racine
-      # (le flake racine doit exporter nixosModules = { default = ./modules/default; };)
       glfDefaultModules = glfRoot.nixosModules.default;
 
     in
     {
       # Définit la configuration NixOS que Calamares doit installer
       nixosConfigurations."GLF-OS" = nixpkgs.lib.nixosSystem {
-        # Paquets de base venant de nixpkgs (stable 24.11)
-        inherit pkgs;
+        inherit pkgs; # Paquets de base venant de nixpkgs (stable 24.11)
 
-        # Arguments spéciaux à passer aux modules (venant du flake racine)
+        # Arguments spéciaux pour les modules (venant du flake racine)
         specialArgs = {
-           # Passer le jeu de paquets unstable correctement défini
-           inherit pkgs-unstable;
-           # On pourrait aussi passer l'input racine si les modules en ont besoin
-           # inherit glfRoot;
+           inherit pkgs-unstable; # Passer le jeu de paquets unstable correctement défini
         };
 
         modules = [
           # Configuration locale générée par Calamares sur la cible
-          # (Ce fichier contient les choix de l'utilisateur : partitionnement, user, etc)
           ./configuration.nix
+          # Important: Le configuration.nix généré par main.py importe déjà hardware-configuration.nix
 
           # Modules GLF venant du flake racine (qui gère pkgs-unstable via specialArgs)
           glfDefaultModules
