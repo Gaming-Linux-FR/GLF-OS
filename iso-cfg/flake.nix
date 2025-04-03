d23942719e9ef9d503becbb24b0a1e70ae52331d
Vinceff <101025517+vinceff@users.noreply.github.com>
2025-04-03 08:43:33 +0200

    Update flake.nix

--- iso-cfg/flake.nix
+++ iso-cfg/flake.nix
@@ -5,49 +5,34 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    # Input pointant vers le dépôt EXTERNE qui contient les modules GLF
-    # Assurez-vous que ce dépôt/branche exporte bien nixosModules.default !
-    glf = { url = "github:vinceff/GLF-OS-stable/glf-os-stable"; follows = "nixpkgs"; };
+    glf.url = "github:vinceff/GLF-OS-stable/glf-os-stable";
   };
 
   outputs =
     {
       nixpkgs,
       nixpkgs-unstable,
-      glf, # Input externe GLF
+      glf, 
       self,
       ...
     }:
+
     let
-      system = "x86_64-linux";
-      nixpkgsConfig = { allowUnfree = true; };
-
-      pkgs = import nixpkgs { inherit system; config = nixpkgsConfig; };
-
-      unstablePkgsImport = import nixpkgs-unstable { inherit system; config = nixpkgsConfig; };
-      pkgs-unstable = unstablePkgsImport; # Correction legacyPackages
-
-      # Récupère les modules depuis l'input EXTERNE 'glf' via 'let' pour éviter pb de scope
-      # **REQUIERT** que le flake 'glf' exporte nixosModules.default
-      glfDefaultModules = glf.nixosModules.default;
-
+      pkgsSettings =
+        system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
     in
     {
       nixosConfigurations."GLF-OS" = nixpkgs.lib.nixosSystem {
-         inherit system pkgs; # Base stable
-
-         # Passer pkgs-unstable aux modules venant de 'glf'
-         specialArgs = {
-            inherit pkgs-unstable;
-            # On pourrait aussi passer 'glf' si les modules en ont besoin
-            # inherit glf;
-         };
-
-         modules = [
-           ./configuration.nix # Généré par Calamares
-           glfDefaultModules   # Modules venant du dépôt GitHub externe
-         ];
+        pkgs = pkgsSettings "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          inputs.glf.nixosModules.default
+        ];
       };
     };
+
 }
