d3cab527a92a5585acbbdab6bdf8a334d2cd4c51
Cammi <118620964+camini@users.noreply.github.com>
2025-04-26 21:47:48 +0200

    Update update.nix

--- modules/default/update.nix
+++ modules/default/update.nix
@@ -22,8 +22,7 @@
     environment.etc."glfos/update.sh" = {
       text = ''
         #!${pkgs.bash}/bin/bash
-        NH_FLAKE_PATH="/etc/nixos"
-        #FLAKE_PATH="/etc/nixos"
+        FLAKE_PATH="/etc/nixos"
         FLAKE_NAME="GLF-OS"
 
         FLAKE_LOCK_PATH="/etc/nixos/flake.lock"
