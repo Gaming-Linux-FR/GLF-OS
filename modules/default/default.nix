8bb450b2ac01c3b74fba182b8d1fd7dca5aa2787
Vinceff <101025517+vinceff@users.noreply.github.com>
2025-05-04 16:35:41 +0200

    Mini update

--- modules/default/default.nix
+++ modules/default/default.nix
@@ -1,27 +1,28 @@
 {
 
-  imports = [
-    ./debug.nix
-    ./aliases.nix
-    ./boot.nix
-    ./branding.nix
-    ./environment.nix
-    ./firefox.nix
-    ./fstrim.nix
-    ./gaming.nix
-    ./nh.nix
-    ./nvidia.nix
-    ./packages.nix
-    ./pipewire.nix
-    ./printing.nix
-    ./system.nix
-    ./update.nix
-    ./version.nix
-    ./standBy.nix
-    ./GLFfetch.nix
-    ./nix-disk-manager.nix
-    ./glfos-environment-selection.nix
-    ./glfos-mangohud-configuration.nix
-  ];
+imports =
+    [
+      ./debug.nix
+      ./aliases.nix
+      ./boot.nix
+      ./branding.nix
+      ./environment.nix
+      ./firefox.nix
+      ./fstrim.nix
+      ./nh.nix
+      ./nvidia.nix
+      ./packages.nix
+      ./pipewire.nix
+      ./printing.nix
+      ./system.nix
+      ./update.nix
+      ./version.nix
+      ./standBy.nix
+      ./GLFfetch.nix
+      ./nix-disk-manager.nix
+      ./glfos-environment-selection.nix
+      ./glfos-mangohud-configuration.nix
+    ]
+    ++ lib.optional (edition != "mini") ./gaming.nix;
 
 }
