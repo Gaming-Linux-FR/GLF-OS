590df46a794d94cd06897d913642f131999ae5ca
Cammi <118620964+camini@users.noreply.github.com>
2025-05-04 15:21:48 +0200

    Update default.nix

--- pkgs/glfos-branding/default.nix
+++ pkgs/glfos-branding/default.nix
@@ -18,13 +18,16 @@ stdenvNoCC.mkDerivation rec {
 
     for SIZE in 16 32 48 64 128 256; do
       mkdir -p $out/share/icons/hicolor/''${SIZE}x''${SIZE}/emblems
-      mkdir -p $out/share/backgrounds/glf
       cp $src/logo/logo-$SIZE.png $out/share/icons/hicolor/''${SIZE}x''${SIZE}/emblems/glfos-logo.png
       cp $src/logo/logo_light-$SIZE.png $out/share/icons/hicolor/''${SIZE}x''${SIZE}/emblems/glfos-logo-light.png
-      cp $src/wallpaper/leather-glf.png $out/share/backgrounds/glf/leather-glf.png
+      
     done
-  '';
   
+    #wallpaper
+      mkdir -p $out/share/backgrounds/glf
+      cp $src/wallpaper/leather-glf.png $out/share/backgrounds/glf/leather-glf.png
+  '';
+
   meta = {
     description = "GLF-OS branding";
     homepage = "https://github.com/Gaming-Linux-FR/GLF-OS";
