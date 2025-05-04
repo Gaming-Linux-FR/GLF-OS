76b12f5d28cd539275460777593ef6ecdcf41318
Cammi <118620964+camini@users.noreply.github.com>
2025-05-04 15:17:26 +0200

    Update default.nix

--- pkgs/glfos-branding/default.nix
+++ pkgs/glfos-branding/default.nix
@@ -18,6 +18,7 @@ stdenvNoCC.mkDerivation rec {
 
     for SIZE in 16 32 48 64 128 256; do
       mkdir -p $out/share/icons/hicolor/''${SIZE}x''${SIZE}/emblems
+      mkdir -p $out/share/backgrounds/gnome
       cp $src/logo/logo-$SIZE.png $out/share/icons/hicolor/''${SIZE}x''${SIZE}/emblems/glfos-logo.png
       cp $src/logo/logo_light-$SIZE.png $out/share/icons/hicolor/''${SIZE}x''${SIZE}/emblems/glfos-logo-light.png
       cp $src/wallpaper/leather-glf.png $out/share/backgrounds/gnome/leather-glf.png
