1dd7f7fd2b76a02e466e5549fcd3f438f2de0996
Cammi <118620964+camini@users.noreply.github.com>
2025-05-05 08:33:56 +0200

    Update default.nix

--- pkgs/glfos-branding/default.nix
+++ pkgs/glfos-branding/default.nix
@@ -7,6 +7,28 @@
   ...
 }:
 
+stdenvNoCC.mkDerivation rec {
+  pname = "glfos-branding";
+  version = "1.0.0"; ### To update version number
+
+  src = ../../assets;
+  
+  buildInputs = [ bash coreutils ];
+
+  installPhase = ''
+    # Logo
+
+    for SIZE in 16 32 48 64 128 256; do
+      mkdir -p $out/share/icons/hicolor/''${SIZE}x''${SIZE}/emblems
+      cp $src/logo/logo-$SIZE.png $out/share/icons/hicolor/''${SIZE}x''${SIZE}/emblems/glfos-logo.png
+      cp $src/logo/logo_light-$SIZE.png $out/share/icons/hicolor/''${SIZE}x''${SIZE}/emblems/glfos-logo-light.png
+      
+    done
+  
+    #wallpaper
+      mkdir -p $out/share/backgrounds/glf
+      cp $src/wallpaper/leather-glf.png $out/share/backgrounds/glf/leather-glf.png
+
 let
   inherit (lib)
     mkOption
@@ -36,35 +58,13 @@ nixos-background-info = pkgs.writeTextFile rec {
 destination = "/share/gnome-background-properties/leather-glf.xml";
   };
 in
-{
-stdenvNoCC.mkDerivation rec {
-  pname = "glfos-branding";
-  version = "1.0.0"; ### To update version number
-
-  src = ../../assets;
-  
-  buildInputs = [ bash coreutils ];
-
-  installPhase = ''
-    # Logo
-
-    for SIZE in 16 32 48 64 128 256; do
-      mkdir -p $out/share/icons/hicolor/''${SIZE}x''${SIZE}/emblems
-      cp $src/logo/logo-$SIZE.png $out/share/icons/hicolor/''${SIZE}x''${SIZE}/emblems/glfos-logo.png
-      cp $src/logo/logo_light-$SIZE.png $out/share/icons/hicolor/''${SIZE}x''${SIZE}/emblems/glfos-logo-light.png
-      
-    done
-  
-    #wallpaper
-      mkdir -p $out/share/backgrounds/glf
-      cp $src/wallpaper/leather-glf.png $out/share/backgrounds/glf/leather-glf.png
-
 
   meta = {
     description = "GLF-OS branding";
     homepage = "https://github.com/Gaming-Linux-FR/GLF-OS";
     license = lib.licenses.agpl3Plus;
   };
-
-};
 }
+
+
+
