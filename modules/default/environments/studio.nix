47eef45d66b7d657be659937b8d8205db5394430
Cammi <118620964+camini@users.noreply.github.com>
2025-05-07 08:44:59 +0200

    Update studio.nix

--- modules/default/environments/studio.nix
+++ modules/default/environments/studio.nix
@@ -7,17 +7,20 @@
 
 {
     config = lib.mkIf(config.glf.environment.enable && (config.glf.environment.edition == "studio" || config.glf.environment.edition == "studio-pro")) {
-          hardware.graphics = {
-            enable = true; 
-            extraPackages = with pkgs; [
-            mesa.opencl # Assure que l'implémentation OpenCL de Mesa (Rusticl) est installée
-            ];
-          };
+        hardware.amdgpu.opencl.enable = true;
 
-        environment.variables = {
-          ROC_ENABLE_PRE_VEGA = "1";
-          RUSTICL_ENABLE = "radeonsi"; 
-        };
+
+        #hardware.graphics = {
+        #    enable = true; 
+        #    extraPackages = with pkgs; [
+        #    mesa.opencl # Assure que l'implémentation OpenCL de Mesa (Rusticl) est installée
+        #    ];
+        #  };
+
+        #environment.variables = {
+        #  ROC_ENABLE_PRE_VEGA = "1";
+        #  RUSTICL_ENABLE = "radeonsi"; 
+        #};
     
     environment.systemPackages =
       if config.glf.environment.edition == "studio-pro" then
