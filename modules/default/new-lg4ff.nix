84b5dd7964518267959526c95bb3f45eb0753c32
Vinceff <101025517+vinceff@users.noreply.github.com>
2025-05-16 16:06:16 +0200

    Update new-lg4ff.nix

--- modules/default/new-lg4ff.nix
+++ modules/default/new-lg4ff.nix
@@ -5,28 +5,23 @@
   ...
 }:
 let
-  cfg = config.hardware.new-lg4ff;
   kernelPackages = config.boot.kernelPackages;
-  new-lg4ff_vff = config.boot.kernelPackages.callPackage ./new-lg4ff {};
-    all-users = builtins.attrNames config.users.users;
-    normal-users = builtins.filter (user: config.users.users.${user}.isNormalUser == true) all-users;
+  new_lg4ff_vff_module = kernelPackages.callPackage ./new-lg4ff {};
 in
 {
-  options.hardware.new-lg4ff = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = ''
-        Enables improved Linux module drivers for Logitech driving wheels.
-        This will replace the existing in-kernel hid-logitech modules.
-        Works most notably on the Logitech G25, G27, G29 and Driving Force (GT).
-      '';
-    };
+  options.hardware.new-lg4ff_vff.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = false;
+    description = ''
+      Enables the experimental new-lg4ff driver (version 0.4.1 depuis Git)
+      avec le support du retour de force (VFF) pour les volants Logitech.
+      Ceci remplace les modules hid-logitech du noyau.
+    '';
   };
 
   config = lib.mkIf config.hardware.new-lg4ff_vff.enable {
     boot = {
-      extraModulePackages = [ new-lg4ff_vff ];
+      extraModulePackages = [ new_lg4ff_vff_module ];
       kernelModules = [ "hid-logitech-new" ];
     };
   };
