5c5e04c85b93c45686291a4f2471020cb780aa1a
Cammi <118620964+camini@users.noreply.github.com>
2025-05-18 07:52:00 +0200

    Update studio.nix

--- modules/default/environments/studio.nix
+++ modules/default/environments/studio.nix
@@ -7,33 +7,31 @@
 
 {
     config = lib.mkIf(config.glf.environment.enable && (config.glf.environment.edition == "studio" || config.glf.environment.edition == "studio-pro")) {
-hardware.amdgpu.opencl.enable = true;
+systemd.tmpfiles.rules = 
+  let
+    rocmEnv = pkgs.symlinkJoin {
+      name = "rocm-combined";
+      paths = with pkgs.rocmPackages; [
+        rocblas
+        hipblas
+        clr
+      ];
+    };
+  in [
+    "L+    /opt/rocm/hip  -    -    -     -    ${rocmEnv}"
+  ];  
 
-#systemd.tmpfiles.rules = 
-#  let
-#    rocmEnv = pkgs.symlinkJoin {
-#      name = "rocm-combined";
-#      paths = with pkgs.rocmPackages; [
-#        rocblas
-#        hipblas
-#        clr
-#      ];
-#    };
-#  in [
-#    "L+    /opt/rocm/hip  -    -    -     -    ${rocmEnv}"
-#  ];  
+        hardware.graphics = {
+            enable = true; 
+            extraPackages = with pkgs; [
+            mesa.opencl # Assure que l'implémentation OpenCL de Mesa (Rusticl) est installée
+            ];
+          };
 
-#        hardware.graphics = {
-#            enable = true; 
-#            extraPackages = with pkgs; [
-#            mesa.opencl # Assure que l'implémentation OpenCL de Mesa (Rusticl) est installée
-#            ];
-#          };
-
-#        environment.variables = {
-#          ROC_ENABLE_PRE_VEGA = "1";
-#          RUSTICL_ENABLE = "radeonsi"; 
-#        };
+        environment.variables = {
+          ROC_ENABLE_PRE_VEGA = "1";
+          RUSTICL_ENABLE = "radeonsi"; 
+        };
     
     environment.systemPackages =
       if config.glf.environment.edition == "studio-pro" then
