ae4cc8ac09397711caabf795ad4079c429ed6c87
Vinceff <101025517+vinceff@users.noreply.github.com>
2025-05-30 21:43:19 +0200

    WARN XONE DESACTIVATED

--- modules/default/gaming.nix
+++ modules/default/gaming.nix
@@ -55,7 +55,7 @@ in
     hardware.fanatec.enable = true;
     hardware.new-lg4ff_vff.enable = true;
     hardware.steam-hardware.enable = true;
-    hardware.xone.enable = true;
+    #hardware.xone.enable = true;
     hardware.xpadneo.enable = true;
     programs.steam.gamescopeSession.enable = true;
     
