4bef7714806701302d239a7cf272ef9e2ba8b433
Cammi <118620964+camini@users.noreply.github.com>
2025-06-08 18:01:49 +0200

    Update gnome.nix

--- modules/default/environments/gnome.nix
+++ modules/default/environments/gnome.nix
@@ -31,9 +31,20 @@ in
 
     documentation.nixos.enable = false;
 
+    #gnome-remote-desktop
     systemd.services."getty@tty1".enable = false;
     systemd.services."autovt@tty1".enable = false;
+    services.xrdp.enable = true;
+    services.xrdp.defaultWindowManager = "${pkgs.gnome.gnome-session}/bin/gnome-session";
+    services.xrdp.openFirewall = true;
 
+    # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
+    # If no user is logged in, the machine will power down after 20 minutes.
+    systemd.targets.sleep.enable = false;
+    systemd.targets.suspend.enable = false;
+    systemd.targets.hibernate.enable = false;
+    systemd.targets.hybrid-sleep.enable = false;
+    
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Packages système
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -52,7 +63,6 @@ in
         vimix-cursors
 
         # gnome
-        gnome-remote-desktop
         gnome-tweaks
         ptyxis
 
