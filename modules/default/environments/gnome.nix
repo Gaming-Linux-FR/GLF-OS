ab34b6c29ef248476a40eaaabf0a4778a34abeed
Cammi <118620964+camini@users.noreply.github.com>
2025-06-10 16:19:00 +0200

    Update gnome.nix

--- modules/default/environments/gnome.nix
+++ modules/default/environments/gnome.nix
@@ -32,18 +32,9 @@ in
     documentation.nixos.enable = false;
     xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
     xdg.portal.config.common.default = "gtk";
-    
+    systemd.services."getty@tty1".enable = false;
     systemd.services."autovt@tty1".enable = false;
-    services.xrdp.enable = true;
-    services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
-    services.xrdp.openFirewall = true;
-
-    # Disable the GNOME3/GDM auto-suspend feature that cannot be disabled in GUI!
-    # If no user is logged in, the machine will power down after 20 minutes.
-    systemd.targets.sleep.enable = false;
-    systemd.targets.suspend.enable = false;
-    systemd.targets.hibernate.enable = false;
-    systemd.targets.hybrid-sleep.enable = false;
+    
     
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Packages système
