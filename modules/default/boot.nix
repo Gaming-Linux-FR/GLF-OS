b101509618ad050e6b80f94cd63695d557074572
Vinceff <101025517+vinceff@users.noreply.github.com>
2025-06-14 14:55:12 +0200

    Update boot.nix

--- modules/default/boot.nix
+++ modules/default/boot.nix
@@ -19,6 +19,7 @@ in
     boot.loader.grub.splashImage = ../../assets/wallpaper/dark.jpg;
     boot.loader.grub.default = "saved";
     boot = {
+      kernelPackages = pkgs.linuxPackages_6_14;
       tmp.cleanOnBoot = true;
       supportedFilesystems.zfs = lib.mkForce false; # Force disable ZFS
       kernelParams =
