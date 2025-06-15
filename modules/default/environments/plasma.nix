5ae9477e935c5ca71bc68a18da8ff93f517d3081
Vinceff <101025517+vinceff@users.noreply.github.com>
2025-06-15 09:38:51 +0200

    add partition manager & kpmcore

--- modules/default/environments/plasma.nix
+++ modules/default/environments/plasma.nix
@@ -1,15 +1,11 @@
-
 {
   lib,
   config,
   pkgs,
   ...
 }:
-
 {
-
   config = lib.mkIf (config.glf.environment.enable && config.glf.environment.type == "plasma") {
-
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Activation de Plasma
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -23,38 +19,32 @@
       };
       desktopManager.plasma6.enable = true;
     };
-
     hardware.bluetooth.enable = true;
-
     xdg.portal = {
       enable = true;
       extraPortals = [ pkgs.kdePackages.xdg-desktop-portal-kde ];
       xdgOpenUsePortal = true;
     };
-
     systemd.services."getty@tty1".enable = false;
     systemd.services."autovt@tty1".enable = false;
-
     documentation.nixos.enable = false;
-
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # Packages système
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     programs.kdeconnect.enable = true;
-
-    #systemPackages = with pkgs; [
-    #  kdePackages.partitionmanager
-    #  kdePackages.kpmcore
-    #  ];
-
     environment = {
       plasma6.excludePackages = [ pkgs.kdePackages.discover ];
-      systemPackages = [
-        (pkgs.writeTextDir "share/sddm/themes/breeze/theme.conf.user" ''
+      systemPackages = with pkgs; [
+        # Packages KDE
+        kdePackages.partitionmanager
+        kdePackages.kpmcore
+        
+        # Configuration SDDM
+        (writeTextDir "share/sddm/themes/breeze/theme.conf.user" ''
           [General]
           background=/etc/wallpapers/glf/white.jpg
         '')
-        ];
+      ];
     };
   };
 }
