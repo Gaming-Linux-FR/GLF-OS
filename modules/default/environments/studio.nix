b61b9f7c5336ca7e41b97d93f4d25fb5b679b8be
Cammi <118620964+camini@users.noreply.github.com>
2025-06-18 11:51:51 +0200

    Update studio.nix

--- modules/default/environments/studio.nix
+++ modules/default/environments/studio.nix
@@ -8,7 +8,16 @@
 
 {
     config = lib.mkIf(config.glf.environment.enable && (config.glf.environment.edition == "studio" || config.glf.environment.edition == "studio-pro")) {
-
+systemd.services.flatpak-repo = {
+      wantedBy = [ "multi-user.target" ];
+      requires = [ "network-online.target" ];
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+      path = [ pkgs.flatpak ];
+      script = ''
+        	flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo && flatpak install -y flathub org.dupot.easyflatpak
+      '';
+    };
 systemd.tmpfiles.rules = 
   let
     rocmEnv = pkgs.symlinkJoin {
