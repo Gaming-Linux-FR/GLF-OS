71a9f4a850a051d2bfaeb58e69600deae64461ba
Cammi <118620964+camini@users.noreply.github.com>
2025-06-21 07:37:51 +0200

    Update default.nix

--- pkgs/nix-disk-manager/default.nix
+++ pkgs/nix-disk-manager/default.nix
@@ -25,7 +25,7 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "nix-disk-manager";
-  version = "1.2.4";
+  version = "1.2.3";
 
   src = fetchFromGitHub {
     owner = "Gaming-Linux-FR";
