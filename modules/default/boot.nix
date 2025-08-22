02acb93d13b952f2fe5351c913d6f55155b62996
Davius <66792063+Xdavius@users.noreply.github.com>
2025-08-22 16:28:02 +0200

    Update boot.nix

--- modules/default/boot.nix
+++ modules/default/boot.nix
@@ -55,6 +55,8 @@ in
         --target=x86_64-efi \
         --efi-directory=/boot/efi \
         --bootloader-id=glf-os \
+        --directory=${pkgs.grub2}/lib/grub/x86_64-efi \
+        --force \
         --recheck
       '';
   };
